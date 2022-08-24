b67e0b744bb2a878fe795ed250cf5ee3de4cac77
michaellimair <michaellimbong@gmail.com>
2022-08-25 00:07:35 +0800

    fix: phone zoom

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -6,6 +6,7 @@ export const PageContainer: FC<any> = ({ children }) => (
     <Head>
       <title>Registration | InDome 2022 - Roof Rave Party</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     </Head>
 
     <main className="flex w-full flex-1 flex-col items-center text-left max-w-8xl">
