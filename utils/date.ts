c67c045ee9a33eb8c8e769ce8c067fd34e8c50cc
Michael Lim <michaellimbong@gmail.com>
2023-09-10 23:38:00 +0800

    fix date formatting

--- utils/date.ts
+++ utils/date.ts
@@ -1,2 +1,2 @@
-const dateTimeFormat = new Intl.DateTimeFormat('en-GB', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
+const dateTimeFormat = new Intl.DateTimeFormat('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
 export const formatDate = (dt: Date) => dateTimeFormat.format(dt);
